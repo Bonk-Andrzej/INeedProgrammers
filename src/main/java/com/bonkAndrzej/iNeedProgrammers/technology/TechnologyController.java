725bfbfc45bda937b1328c4234bf6423b5ebbb12
Andrzej <bonkandrzej09@gmail.com>
2019-10-27 15:28:39 +0100

    Added service and controller for technology management.

--- src/main/java/com/bonkAndrzej/iNeedProgrammers/technology/TechnologyController.java
+++ src/main/java/com/bonkAndrzej/iNeedProgrammers/technology/TechnologyController.java
@@ -1,14 +1,68 @@
 package com.bonkAndrzej.iNeedProgrammers.technology;
 
+import com.bonkAndrzej.iNeedProgrammers.technology.dto.TechnologyDto;
+import com.bonkAndrzej.iNeedProgrammers.technology.dto.TechnologyForm;
+import com.bonkAndrzej.iNeedProgrammers.technology.exception.TechnologyException;
+import com.bonkAndrzej.iNeedProgrammers.user.role.roleAnnotation.Admin;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
 
 @Slf4j
 @RestController @Validated
 @AllArgsConstructor
 @RequestMapping("/api")
 public class TechnologyController {
+
+    private final TechnologyService technologyService;
+
+    @Admin
+    @PostMapping("/categories")
+    public ResponseEntity<TechnologyDto> createTechnology(@Valid @RequestBody TechnologyForm technologyForm) {
+        TechnologyDto technologyDto = technologyService.createTechnology(technologyForm);
+
+        return new ResponseEntity<>(technologyDto, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<TechnologyDto> getTechnology(@Positive @PathVariable Long id) throws TechnologyException {
+        TechnologyDto technologyDto = technologyService.getTechnology(id);
+
+        return new ResponseEntity<>(technologyDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<TechnologyDto>> getAllTechnologies() {
+        List<TechnologyDto> technologyDtos = technologyService.getAllTechnologies();
+
+        return new ResponseEntity<>(technologyDtos, HttpStatus.OK);
+    }
+
+    @Admin
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<TechnologyDto> updateTechnology(@Valid @RequestBody TechnologyForm technologyForm, @Positive @PathVariable Long id)
+            throws TechnologyException {
+        TechnologyDto technologyDto = technologyService.updateTechnology(technologyForm, id);
+
+        return new ResponseEntity<>(technologyDto, HttpStatus.OK);
+    }
+
+    @Admin
+    @DeleteMapping("/categories/{id}")
+    public ResponseEntity<Void> deleteTechnology(@Positive @PathVariable Long id, @PositiveOrZero @RequestParam Integer version)
+            throws TechnologyException {
+        technologyService.deleteTechnology(id, version);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/bonkAndrzej/iNeedProgrammers/technology/TechnologyService.java
+++ src/main/java/com/bonkAndrzej/iNeedProgrammers/technology/TechnologyService.java
@@ -1,13 +1,70 @@
 package com.bonkAndrzej.iNeedProgrammers.technology;
 
+import com.bonkAndrzej.iNeedProgrammers.technology.dto.TechnologyDto;
+import com.bonkAndrzej.iNeedProgrammers.technology.dto.TechnologyForm;
+import com.bonkAndrzej.iNeedProgrammers.technology.exception.TechnologyException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class TechnologyService {
+
+    private final TechnologyRepository technologyRepository;
+
+    public TechnologyDto createTechnology(TechnologyForm technologyForm) {
+        Technology technology = new Technology();
+        technology.setName(technologyForm.getName());
+
+        Technology technologyAfterSave = technologyRepository.save(technology);
+        return new TechnologyDto(technologyAfterSave);
+    }
+
+    public TechnologyDto updateTechnology(TechnologyForm technologyForm, Long id) throws TechnologyException {
+        Technology technology = technologyRepository.findById(id).orElseThrow(
+                () -> new TechnologyException("Technology not found with given id " + id));
+
+        if (!technology.getVersion().equals(technologyForm.getVersion()))
+            throw new TechnologyException("OptimisticLockException - wrong technology version " + technologyForm.getVersion() +
+                    "\nExpected " + technology.getVersion());
+
+        technology.setName(technologyForm.getName());
+        Technology technologyAfterUpdate = technologyRepository.save(technology);
+        return new TechnologyDto(technologyAfterUpdate);
+    }
+
+    @Transactional(readOnly = true)
+    public TechnologyDto getTechnology(Long id) throws TechnologyException {
+        Technology technology = technologyRepository.findById(id).orElseThrow(
+                () -> new TechnologyException("Technology not found with given id " + id));
+
+        return new TechnologyDto(technology);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TechnologyDto> getAllTechnologies() {
+        List<Technology> categories = technologyRepository.findAll();
+
+        return categories.stream()
+                .map(TechnologyDto::new).collect(Collectors.toList());
+    }
+
+    public void deleteTechnology(Long id, Integer version) throws TechnologyException {
+        Technology technology = technologyRepository.findById(id).orElseThrow(
+                () -> new TechnologyException("Technology not found with given id " + id));
+
+        if (!technology.getVersion().equals(version))
+            throw new TechnologyException("OptimisticLockException - wrong technology version " + version +
+                    "\nExpected " + technology.getVersion());
+
+        technologyRepository.delete(technology);
+    }
 
 
 }
